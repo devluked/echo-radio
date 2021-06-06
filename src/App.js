b6ec3f6343302ece99c9fa6538d0381ce9e6b8e8
Luke Duggan <48234476+devluked@users.noreply.github.com>
2021-06-05 18:48:54 -0700

    basic functionality done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import ReactPlayer from 'react-player'
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ToggleStreamEpisodes/>
+   </div>
   );
+}
+
+
+function ToggleStreamEpisodes() {
+  const [displayVar, toggleDisplay] = useState(false)
+
+  const toggleViewHandler = () => {
+    toggleDisplay(!displayVar)
+  }
+  if (displayVar===false) {
+    return (
+      <div className="streampage">
+        <button className="episodestogglebtn" onClick={toggleViewHandler}>Episodes</button>
+        <StreamPlayer/>
+     </div>
+    )
+  } else 
+  return (
+    <div className="episodeContainer">
+      <button className="episodestogglebtn" onClick={toggleViewHandler}>Episodes</button>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/echo-radio-008"/>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/echo-radio-007"/>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/echo-radio-006"/>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/echo-radio-005"/>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/echo-radio-004"/>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/echo-radio-003"/>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/episode-002"/>
+      <EpisodesComponent url="https://soundcloud.com/echoradioshow/episode-001"/>
+      
+
+    </div>
+  )
+}
+
+function StreamPlayer() {
+  const [isPlaying, togglePlaying] = useState(false)
+
+
+  const handlePlay = () => {
+    togglePlaying(!isPlaying)
+  }
+
+  return (
+  <div className="playbtnep">
+    <button className="player" onClick={handlePlay}>
+    <div className="play"></div>
+    <h1 className="title">Echo Radio</h1>
+    </button>
+    <ReactPlayer height="200" width="200" playing={isPlaying} url="https://soundcloud.com/echoradioshow/echo-radio-008"/>
+  </div>
+  )}
+
+
+function EpisodesComponent(props) {
+  return (
+    <div className="episodes">
+      <ReactPlayer height="100" width="100" url={props.url}/>
+    </div>
+  )
+
 }
 
 export default App;
